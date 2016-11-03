fda5f7ac8ad8e0f3fbd0db1ce7a6645305542f7b
root <root@Arrocero.localdomain>
2016-11-03 00:50:12 -0300

    funkando muy lentamente...

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 /* -------------------------------------------------------
 Cargador:   
     carga yamls a drupal 8 mediante la gracia de Spyc.
+
+drush9 -v scr a.php
+
 ------------------------------------------------------- */
-$debug = 0;
+$debug = 1;
 $drupal = 1;
 
 
@@ -13,51 +16,35 @@ use Drupal\file\Entity\File;
 
 require_once "./d8-charger-yaml/y.php";
 
-
-
 $archivos_ruta = './d8-charger-yaml/yamls/';
-//$archivos_yaml = scandir($archivos_ruta);
 $archivos_yaml = file_scan_directory($archivos_ruta, '/[.*.y.?ml]$/');
 
 foreach ($archivos_yaml as $y){
-    //if (preg_match('/\.y.?ml$/',$y)){
-    //$Coso = spyc_load_file($archivos_ruta . $y);
         $Coso = spyc_load_file($y->uri);
         if ($debug){
-            print "<span>" . $y . "</span>";
-            print "<pre>";
-            print_r( $Coso );
-            print "</pre>";
-            print "<hr />";
+            drush_log( $Coso,'ok');
         }
 
     if ($drupal){
-        //mandar cosas a Drupal
+        $t = array_keys($Coso);
+        $tt = $t[0];
         $node = Node::create([
-                //'language'             => LANGUAGE_NONE,
+            'language'             => 'LANGUAGE_NONE',
             'type'                 => 'producto',
-            'title'                => $Coso['CODIGO'],
-            'field_codigo_interno' => $Coso['CODIGO-INTERNO'],
-            'field_descripcion'    => $Coso['DESCRIPCION'],
-            'field_tags'           => $Coso['ETIQUETAS'],
-            'field_imagen'         => $Coso['IMAGEN'],
-            'field_linea'          => $Coso['LINEA'],
-            'field_nombre'         => $Coso['NOMBRE'],
-            'field_presentacion'   => $Coso['PRESENTACION'],
-            'field_tipo'           => $Coso['TIPO'],
+            'title'                => $tt,
+            'field_codigo_interno' => $Coso[$tt]['CODIGO-INTERNO'],
+            'field_descripcion'    => $Coso[$tt]['DESCRIPCION'],
+            'field_nombre'         => $Coso[$tt]['NOMBRE'],
+            'field_presentacion'   => $Coso[$tt]['PRESENTACION'],
+            // ---------------------------------------------
+            'field_tipo'           => $Coso[$tt]['TIPO'],
+            'field_tags'           => $Coso[$tt]['ETIQUETAS'],
+            'field_linea'          => $Coso[$tt]['LINEA'],
+            // ---------------------------------------------
+            'field_imagen'         => $Coso[$tt]['IMAGEN'],
         ]);
         $node->save();
     }
-    
-    
-    //foreach ($yaml['categories'] as $category) {
-        //$term = reset(taxonomy_get_term_by_name($category));
-        //$node->field_category[$node->language][]['tid'] = $term->tid;
-    //}
-    //$node->body[ $node->language ][0]['value'] = $Coso['body'];
-
-    
-    //}
 }
 
 
