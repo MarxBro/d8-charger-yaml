f1ecffc4b92f3d014851c4ea78846ebaaeda149e
MarxBro <allthemarxbroters@gmail.com>
2016-11-22 23:22:10 -0300

    cambios

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ Cargador:
         drush9 -v scr carpete/index.php
 
 ------------------------------------------------------- */
-$debug = 0;
+$debug = 1;
 $drupal = 1;
 
 
@@ -32,7 +32,7 @@ $archivos_imgs = file_scan_directory($archivos_ruta_imgs, '/[.*.jp.?g]$|[.*.png]
 foreach ($archivos_yaml as $y){
         $Coso = spyc_load_file($y->uri);
         if ($debug){
-            drush_log( print_r($archivos_imgs) ,'ok');
+            //drush_log( print_r($Coso) ,'ok');
         }
 
     if ($drupal){
@@ -42,9 +42,11 @@ foreach ($archivos_yaml as $y){
         // Taxonomias
         $f_linea = taxonomizame_la_nutria($Coso[$tt]['LINEA'], 'lineas'); // Jerarquía
         $f_tags = taxonomizame_la_nutria($Coso[$tt]['ETIQUETAS'], 'tags');
-        $f_tipo = taxonomizame_la_nutria($Coso[$tt]['TIPO'], 'tipo');
+        if(!empty($Coso[$tt]['TIPO'])){
+            $f_tipo = taxonomizame_la_nutria($Coso[$tt]['TIPO'], 'tipo');
+        }
         if ($debug){
-            drush_log(print_r($f_linea),'ok');    
+            //drush_log(print_r($f_linea),'ok');    
         }
 
         //Copiar el archivo al public, registrarlo drupalezcamente y agregarlo al nodo
@@ -69,13 +71,14 @@ foreach ($archivos_yaml as $y){
             'field_nombre'         => array('value'=>$Coso[$tt]['NOMBRE']),
             'field_presentacion'   => array('value'=>$Coso[$tt]['PRESENTACION']),
             //// ---------------------------------------------
-            'field_tipo'           => $f_tipo,
             'field_tags'           => $f_tags,
             'field_linea'          => $f_linea,
+            //'field_tipo'           => $f_tipo ,
             // ---------------------------------------------
             'field_imagen'         => array('target_id'=>$filex->id()),
         ]);
         $node->save();
+        //
     }
 }
 
@@ -84,40 +87,52 @@ function taxonomizame_la_nutria($palabra, $vocabulario){
         $debug = 1;
         $g = [];//$palabra;
         if(is_array($palabra)){
-            foreach($palabra as $a){
-                if (is_array($a)){
-                    array_push($g,$a);
-                } else {
-                    $g[] = $a;
-                }
-            }
+            $jijiji = recursive_implode($palabra,',');
+            $g = split(',',$jijiji);
+            //drush_log(print $jijiji,'ok');    
         } else {
             $g[] = $palabra;
         }
+            drush_log(print_r($g),'ok');    
         
         foreach($g as $tt){
+            if(isset($tt)){
             if ($terms = taxonomy_term_load_multiple_by_name($tt,$vocabulario)){
                 $term = reset($terms);
                 if ($vocabulario == 'lineas'){
-                    if ($term->id() > 10 ){
-                        array_push($ids,array('target_id' => $term->id()));
+                    if ($term->id()>10){
+                    array_push($ids,array('target_id' => $term->id()));
                     }
                 } else{
                     array_push($ids,array('target_id' => $term->id()));
                 }
             } else {
+                if (!($tt == 'Array')){
                 $term = Term::create([
                         'name' => $tt,
                         'vid' => $vocabulario,
                 ]);
                 $term->save();    
                 array_push($ids,array('target_id' => $term->id()));
-            }
+                }
+            }}
         }
         if(!empty($ids)){
             return $ids;
         }
 }
 
+function recursive_implode(array $array, $glue = ',', $include_keys = false, $trim_all = false) {
+	$glued_string = '';
+	// Recursively iterates array and adds key/value to glued string
+	array_walk_recursive($array, function($value, $key) use ($glue, $include_keys, &$glued_string) {
+		$include_keys and $glued_string .= $key.$glue;
+		$glued_string .= $value.$glue;
+	});
+	// Removes last $glue from string
+	strlen($glue) > 0 and $glued_string = substr($glued_string, 0, -strlen($glue));
+	// Trim ALL whitespace
+	$trim_all and $glued_string = preg_replace("/(\s)/ixsm", '', $glued_string);
+	return (string) $glued_string;
+}
 ?>
-
